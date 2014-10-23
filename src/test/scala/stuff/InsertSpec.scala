d92f194d793f7ab35b0a0eadf67738fc0acb1c0b
Roberto Bonvallet <rbonvall@gmail.com>
2014-10-22 23:36:54 -0300

    Use seq of functions to test (not working yet)

--- src/test/scala/stuff/InsertSpec.scala
+++ src/test/scala/stuff/InsertSpec.scala
@@ -5,21 +5,23 @@ import stuff.InsertIntoOrderedList._
 
 class InsertIntoOrderedListSpec extends FunSpec {
 
-  describe("insert") {
+  val functions = Seq(insert1 _)
 
-    it("should insert into correct position of a long list") {
-      val l = List(11, 33, 55, 77, 88)
-      assert(insert1(l,  0) == List( 0, 11, 33, 55, 77, 88))
-      assert(insert1(l, 44) == List(11, 33, 44, 55, 77, 88))
-      assert(insert1(l, 99) == List(11, 33, 55, 77, 88, 99))
-      assert(1 + 1 === 2)
+  functions foreach { (insertFunction) =>
+    describe("insert") {
+      it("should insert into correct position of a long list") {
+        val l = List(11, 33, 55, 77, 88)
+        assert(insertFunction(l,  0) == List( 0, 11, 33, 55, 77, 88))
+        assert(insertFunction(l, 44) == List(11, 33, 44, 55, 77, 88))
+        assert(insertFunction(l, 99) == List(11, 33, 55, 77, 88, 99))
+        assert(1 + 1 === 2)
+      }
+
+      it("should insert into an empty list") {
+        val l = List()
+        assert(insertFunction(l, 22) == List(22))
+        assert(1 + 1 === 2)
+      }
     }
-
-    it("should insert into an empty list") {
-      val l = List()
-      assert(insert1(l, 22) == List(22))
-      assert(1 + 1 === 2)
-    }
-
   }
 }
