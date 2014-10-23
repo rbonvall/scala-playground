9ed9a6b60b4acd8e763425c931653bc5dd3c97de
Roberto Bonvallet <rbonvall@gmail.com>
2014-10-22 23:10:13 -0300

    First implementation of insert

--- src/main/scala/stuff/Insert.scala
+++ src/main/scala/stuff/Insert.scala
@@ -2,6 +2,11 @@ package stuff
 
 object Insert {
 
-  def insert1[T](list: List[T], elem: T): List[T] = ???
+  def insert1[T <% Ordered[T]](list: List[T], elem: T): List[T] = list match {
+    case Nil => List(elem)
+    case x :: xs =>
+      if (elem < x) elem :: x :: xs
+      else          x :: insert1(xs, elem)
+  }
 
 }
