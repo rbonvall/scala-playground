e105b7d03ead5c849bf5a4999e13fd8774c75a75
Roberto Bonvallet <roberto@nimbic.com>
2014-11-05 18:05:35 -0300

    Remove dead code

--- src/main/scala/condparse/parser.scala
+++ src/main/scala/condparse/parser.scala
@@ -2,11 +2,6 @@ package condparse
 
 import scala.util.parsing.combinator._
 
-// trait Parser[+A] extends (Stream[Character] ⇒ Result[A])
-// sealed trait Result[+A]
-// case class Success[+A](value: A, rem: Strem[Character]) extends Result[A]
-// case class Failure(msg: String) extends Result[Nothing]
-
 object CondParser extends RegexParsers {
 
   def name: Parser[String] = """[A-Za-z]+""".r
