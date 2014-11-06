3607dd19234fde8eb5f3b8ece53913441a7fe126
Roberto Bonvallet <rbonvall@gmail.com>
2014-11-06 20:02:42 -0300

    Test to show how parsers can be fed directly

--- src/test/scala/condparse/parserSpec.scala
+++ src/test/scala/condparse/parserSpec.scala
@@ -5,6 +5,28 @@ import CondParser._
 
 class parserSpec extends FunSpec {
 
+  describe("number parser being fed directly") {
+    import scala.util.parsing.input.CharSequenceReader
+    it("parses a number") {
+      val result = number(new CharSequenceReader("-3.14159abc"))
+      assert(result match {
+        case Success(n, rest) => n === -3.14159 &&
+            rest.source.subSequence(8, 11).toString === "abc"
+        case failure          => false
+      })
+    }
+    it("doesn't parse a word") {
+      val result = number(new CharSequenceReader("stuff"))
+      assert(result match {
+        case Success(_, _) => false
+        case failure       => true
+      })
+    }
+    // A less cumbersome way of feeding the parsers
+    // is by using the helper functions
+    // parse(parser, input) and parseAll(parser, input).
+  }
+
   def parsingSucceeds[T](parser: Parser[T], input: String, expected: T) = {
     val output = parseAll(parser, input)
     assert(output.successful && output.get === expected)
