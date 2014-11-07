638f3f220272d03d53562c7714ce01468cf005fb
Roberto Bonvallet <roberto@nimbic.com>
2014-11-07 12:39:27 -0300

    Classes Bound and Interval

--- src/main/scala/condparse/parser.scala
+++ src/main/scala/condparse/parser.scala
@@ -4,6 +4,12 @@ import scala.util.parsing.combinator._
 
 object CondParser extends RegexParsers {
 
+  case class Bound(value: Double, closed: Boolean)
+  case class Interval(left: Option[Bound], right: Option[Bound]) {
+    def this(left: Double, leftClosed: Boolean, right: Double, rightClosed: Boolean) =
+      this(Some(Bound(left, leftClosed)), Some(Bound(right, rightClosed)))
+  }
+
   def name: Parser[String] = """[A-Za-z]+""".r
   def number: Parser[Double] = """[+-]?\d+(\.\d*)?""".r ^^ { _.toDouble }
   def nameList: Parser[Seq[String]] = rep1sep(name, ",")
