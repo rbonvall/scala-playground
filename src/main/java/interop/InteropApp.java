23142e5070efdf08d6bc09d7fb47b26cae74cf0e
Roberto Bonvallet <roberto@nimbic.com>
2014-11-11 12:19:11 -0300

    Example of attribute lookup
    scalac does not generate a getYear method

--- src/main/java/interop/InteropApp.java
+++ src/main/java/interop/InteropApp.java
@@ -8,8 +8,11 @@ public class InteropApp {
     }
     public static void dates() {
         Date today = new Date(1998, 9, 18);
+        System.out.println("Welcome to the year " + today.year());
+
         Date tomorrow = today.next();
         System.out.println("Tomorrow is " + tomorrow);
+
         Date xmas = new Date(1998, 12, 25);
         System.out.println("There are " + (xmas.$minus(today)) + " days left to Christmas.");
     }
