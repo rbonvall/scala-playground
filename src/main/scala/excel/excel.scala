032ebb78fa4f01e379c8b6ede8792b6511d0f0cb
Roberto Bonvallet <rbonvall@gmail.com>
2015-03-07 23:31:16 -0300

    Function for reading xls, yet to be tested

--- src/main/scala/excel/excel.scala
+++ src/main/scala/excel/excel.scala
@@ -1,9 +1,33 @@
 package excel
 
-case class Workbook(fileName: String, sheets: Seq[Sheet])
-case class Sheet(name: String, rows: Seq[Seq[Cell]])
+import org.apache.poi.xssf.usermodel._
+import java.io.{FileInputStream, File}
+
 case class Cell(coords: String, content: String) {
   val coordParts = coords.split("(?<=[A-Za-z])(?=[0-9])")
   val row = coordParts(1)
   val col = coordParts(0)
+}
+case class Sheet(name: String, rows: Seq[Seq[Cell]])
+case class Workbook(fileName: String, sheets: Seq[Sheet])
+
+object excel {
+  def readWorkbook(fileName: String) = {
+    val file: FileInputStream = new FileInputStream(new File(fileName))
+    val xssfWorkbook: XSSFWorkbook = new XSSFWorkbook(file)
+    val sheets = for {
+      n ← 0 until xssfWorkbook.getNumberOfSheets
+      xssfSheet = xssfWorkbook.getSheetAt(n)
+      name = xssfSheet.getSheetName
+      rows = for {
+        i ← 0 until xssfSheet.getPhysicalNumberOfRows
+        xssfRow = xssfSheet.getRow(i)
+        rowCells = for {
+          j ← 0 until xssfRow.getPhysicalNumberOfCells
+          xssfCell = xssfRow.getCell(j)
+        } yield Cell(xssfCell.getReference, xssfCell.getRawValue)
+      } yield rowCells
+    } yield Sheet(name, rows)
+    Workbook(fileName, sheets)
+  }
 }
