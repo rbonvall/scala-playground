a58e54336feef32295b8f6d2590bb032300702b6
Roberto Bonvallet <rbonvall@gmail.com>
2015-03-25 23:22:32 -0400

    Skeleton for rectangle processor

---
+++ src/main/scala/rectangles/CalcRect.scala
@@ -0,0 +1,9 @@
+package rectangles
+
+//import scala.xml.XML
+
+object CalcRect {
+  def main(args: Array[String]) = {
+    //val data = scala.xml.XML.loadFile("src/main/resources/rectangles/r.xml")
+  }
+}
