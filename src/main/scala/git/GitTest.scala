3461694f28792912eb2db224246377f8c3968b95
Roberto Bonvallet <rbonvall@gmail.com>
2015-06-28 23:14:27 -0400

    Basic repo operations, test for committing trees

--- src/main/scala/git/GitTest.scala
+++ src/main/scala/git/GitTest.scala
@@ -1,18 +1,78 @@
 package git
 
-import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib._
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 
-object GitTest {
-  def main(args: Array[String]) {
-    val builder = new FileRepositoryBuilder
-    val repo = builder.setGitDir(new java.io.File("/tmp/gittest"))
-      .readEnvironment()
-      .findGitDir()
-      .build()
+import scala.collection.immutable.ListMap
+import java.io.File
 
-    val head = repo.resolve("HEAD")
-    val HEAD = repo.getRef("refs/heads/master")
+
+case class Tree[T](value: T, children: ListMap[String, Tree[T]])
+
+
+case class Repo(path: String) {
+  val repo = FileRepositoryBuilder.create(new File(s"$path/.git"))
+  val me = new PersonIdent("Roberto Bonvallet", "rbonvall@gmail.com")
+
+  def insertBlob(content: String): ObjectId = {
+    val inserter = repo.newObjectInserter
+    val id = inserter.insert(Constants.OBJ_BLOB, content.getBytes("utf-8"))
+    inserter.flush()
+    id
   }
+
+  def readBlob(id: ObjectId): String = {
+    val loader = repo.newObjectReader.open(id)
+    require(loader.getType == Constants.OBJ_BLOB)
+    new String(loader.getBytes, "utf-8")
+  }
+
+  def insertTree(tree: Tree[String]): ObjectId = {
+    val formatter = new TreeFormatter
+    val blobId = insertBlob(tree.value)
+    formatter.append("VALUE", FileMode.REGULAR_FILE, blobId)
+    for {(n, t) ← tree.children} {
+      val childId = insertTree(t)
+      formatter.append(n, FileMode.TREE, childId)
+    }
+    val inserter = repo.newObjectInserter
+    val id = inserter.insert(formatter)
+    inserter.flush()
+    id
+  }
+
+  def commit(treeId: ObjectId, msg: String): ObjectId = {
+    val builder = new CommitBuilder
+    builder.setTreeId(treeId)
+    builder.setMessage(msg)
+    builder.setAuthor(me)
+    builder.setCommitter(me)
+    val inserter = repo.newObjectInserter
+    val id = inserter.insert(builder)
+    inserter.flush()
+    id
+  }
+
+
+}
+
+object GitTest {
+  def main(args: Array[String]) = {
+  }
+
+  def createRepo(path: String) = {
+    val dir = new File(path)
+    dir.delete()
+
+    Git.init.setDirectory(dir).call
+
+    val f = new File(dir.getAbsolutePath, ".git")
+    val repo = FileRepositoryBuilder.create(f)
+
+    println(s"Created new repo at ${repo.getDirectory}")
+    repo.close()
+  }
+
 }
 
--- src/test/scala/git/GitTestSpec.scala
+++ src/test/scala/git/GitTestSpec.scala
@@ -1,6 +1,51 @@
 package git
 
 import org.scalatest.FunSpec
+import scala.collection.immutable.ListMap
 
 class GitTestSpec extends FunSpec {
+  describe("Git test") {
+    it("works") {
+      val tree1 =
+        Tree("root", ListMap(
+          "first" → Tree("abcde", ListMap(
+            "a" → Tree("lorem", ListMap.empty),
+            "b" → Tree("ipsum", ListMap.empty),
+            "c" → Tree("dolor", ListMap(
+              "cc" → Tree("sit", ListMap.empty)
+            )),
+            "d" → Tree("amet", ListMap.empty)
+          )),
+          "second" → Tree("defghi", ListMap(
+            "x" → Tree("stuff", ListMap.empty),
+            "y" → Tree("guareva", ListMap.empty)
+          ))
+        ))
+      val tree2 =
+        Tree("root", ListMap(
+          "first" → Tree("abcde", ListMap(
+            "a" → Tree("lorem", ListMap.empty),
+            "c" → Tree("dolor", ListMap(
+              "cc" → Tree("sit", ListMap(
+                "second" → Tree("defghi", ListMap(
+                  "x" → Tree("stuff", ListMap.empty),
+                  "y" → Tree("guareva", ListMap.empty)
+                ))
+              ))
+            )),
+            "d" → Tree("amet", ListMap.empty)
+          ))
+        ))
+
+      val path = "/tmp/gittest"
+      GitTest.createRepo(path)
+      val repo = Repo(path)
+      val t1 = repo.insertTree(tree1)
+      val c1 = repo.commit(t1, "First commit")
+      println(s"First commit: $c1")
+      val t2 = repo.insertTree(tree2)
+      val c2 = repo.commit(t2, "Second commit")
+      println(s"Second commit: $c2")
+    }
+  }
 }
