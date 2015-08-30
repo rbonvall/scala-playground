8df51873ac93a036e67ed1779ef7f46d56edc717
Roberto Bonvallet <rbonvall@gmail.com>
2015-08-30 16:38:18 -0400

    Alternative checkDigit function

--- src/main/scala/rut/Rut.scala
+++ src/main/scala/rut/Rut.scala
@@ -6,11 +6,20 @@ object Rut {
     if (n == 0) acc
     else        getDigits(n / 10, n % 10 :: acc)
   
-  def checkDigit(rut: Int): Char = {
-    val reversedDigits = getDigits(rut).reverse
-    val factors = Stream.continually(2 to 7).flatten
-    val s = reversedDigits.zip(factors).map { case (d, f) ⇒ d * f }.sum
-    "0k987654321"(s % 11)
+  // def checkDigit(rut: Int): Char = {
+  //   val reversedDigits = getDigits(rut).reverse
+  //   val factors = Stream.continually(2 to 7).flatten
+  //   val pairs = reversedDigits zip factors
+  //   val s = pairs.map { case (d, f) ⇒ d * f }.sum
+  //   "0k987654321"(s % 11)
+  // }
+
+  def checkDigit(rut: Int) = "0k987654321" {
+    getDigits(rut)
+      .reverse
+      .zip(Stream.continually(2 to 7).flatten)
+      .map { case (d, f) ⇒ d * f }
+      .sum % 11
   }
 
   private val rutPattern = """(\d+)-([0-9Kk])""".r
