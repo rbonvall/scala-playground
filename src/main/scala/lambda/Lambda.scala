c9295929523a076a216dbae0dcf125f914acf224
Roberto Bonvallet <rbonvall@gmail.com>
2015-12-28 23:12:43 -0300

    Var occursFreeIn and occursBoundIn expression

--- src/main/scala/lambda/Lambda.scala
+++ src/main/scala/lambda/Lambda.scala
@@ -1,14 +1,28 @@
 package lambda
 
 sealed trait Expression
-case class Variable(name: String) extends Expression {
-  override def toString = name
-}
+
 case class Application(fn: Expression, arg: Expression) extends Expression {
   override def toString = s"($fn)($arg)"
 }
+
 case class Abstraction(param: Variable, expr: Expression) extends Expression {
   override def toString = s"λ$param.{$expr}"
+}
+
+case class Variable(name: String) extends Expression {
+  override def toString = name
+  def occursFreeIn(expr: Expression): Boolean = expr match {
+    case Variable(y)                 ⇒ name == y
+    case Application(e, f)           ⇒ occursFreeIn(e) || occursFreeIn(f)
+    case Abstraction(Variable(y), e) ⇒ name != y && occursFreeIn(e)
+  }
+  def occursBoundIn(expr: Expression): Boolean = expr match {
+    case Variable(_)                 ⇒ false
+    case Application(e, f)           ⇒ occursBoundIn(e) || occursBoundIn(f)
+    case Abstraction(Variable(y), e) ⇒ (name == y && occursFreeIn(e)) ||
+                                       occursBoundIn(e)
+  }
 }
 
 
